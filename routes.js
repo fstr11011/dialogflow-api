cbc978d5eeefc5ecaf039ac2f81a9fe3fb7fab27
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-06 15:06:06 -0400

    update

--- routes.js
+++ routes.js
@@ -24,7 +24,7 @@ router.post("/", function(req, res, next){
                                 "fulfillmentText": "Hello " + info.name + ". Which of your the following books would you like to suspend?\n" + info.books,
                                 "outputContexts": [
                                     {
-                                      "name": "projects/$api-test-a47a7/agent/sessions/$dialogflow-jeiobw@api-test-a47a7.iam.gserviceaccount.com/contexts/context name",
+                                      "name": "projects/api-test-a47a7/agent/sessions/dialogflow-jeiobw@api-test-a47a7.iam.gserviceaccount.com/contexts/suspend-followup",
                                       "lifespanCount": 5,
                                       "parameters": {
                                         "name": info.name,
