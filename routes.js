1ca6dabd1df31709b82c8e1e23111fb22b8c7fd5
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-06 13:45:59 -0400

    update

--- routes.js
+++ routes.js
@@ -21,61 +21,6 @@ router.post("/", function(req, res, next){
                     res.json(
                         {
                             "fulfillmentText": "Hello " + info.name + " these are your books: " + info.books,
-                            "fulfillmentMessages": [
-                              {
-                                "card": {
-                                  "title": "Account Info",
-                                  "subtitle": "User's name",
-                                  "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                                  "buttons": [
-                                    {
-                                      "text": "button text",
-                                      "postback": "https://assistant.google.com/"
-                                    }
-                                  ]
-                                }
-                              }
-                            ],
-                            "source": "example.com",
-                            "payload": {
-                              "google": {
-                                "expectUserResponse": true,
-                                "richResponse": {
-                                  "items": [
-                                    {
-                                      "simpleResponse": {
-                                        "textToSpeech": "this is a simple response"
-                                      }
-                                    }
-                                  ]
-                                }
-                              },
-                              "facebook": {
-                                "text": "Hello, Facebook! " + info.name
-                              },
-                              "slack": {
-                                "text": "This is a text response for Slack. " + info.name
-                              }
-                            },
-                            "outputContexts": [
-                              {
-                                "name": "projects/api-test-a47a7/agent/sessions/dialogflow-jeiobw@api-test-a47a7.iam.gserviceaccount.com/contexts/context name",
-                                "lifespanCount": 5,
-                                "parameters": {
-                                  "name": info.name,
-                                  "accountNumber": info.accountNumber,
-                                  "email": info.accountNumber,
-                                  "books": info.books
-                                }
-                              }
-                            ],
-                            "followupEventInput": {
-                              "name": "userinfo",
-                              "languageCode": "en-US",
-                              "parameters": {
-                                "param": "param value"
-                              }
-                            }
                           }
                     );
                 });
