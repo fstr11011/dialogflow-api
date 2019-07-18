d002f99934ec25d19cd2433ce6dcd285e56956e5
Fester527 <35502356+Fester527@users.noreply.github.com>
2019-07-18 09:10:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    dbUrl:  "mongodb://mtg:mtg123@ds113942.mlab.com:13942/dialogflow-api",
-    tenancyName: "Roboyo_MGladstein",
-    usernameOrEmailAddress: "admin",
-    password: "Mg200120798"
+    dbUrl:  "",
+    tenancyName: "",
+    usernameOrEmailAddress: "",
+    password: ""
 }
